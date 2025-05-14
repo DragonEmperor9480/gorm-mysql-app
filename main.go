b3f7b6c4228afdd95a0c7a06327f3a3080ed308f
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-05-14 11:32:32 +0530

    Create a table in database

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"gorm.io/gorm"
 )
 
+type User struct {
+	ID    uint `gorm:"primarykey"`
+	Name  string
+	Email string `gorm:"unique"`
+	Age   int
+}
+
 func main() {
 	dsn := "root:0000@tcp(127.0.0.1:3306)/gormdb?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -14,4 +21,11 @@ func main() {
 		panic("Failed to connect to database!")
 	}
 	fmt.Println("Database connected successfully!", db)
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic("Failed to auto-migrate schema!")
+	}
+	fmt.Println("✅ Migration complete!")
+
 }
